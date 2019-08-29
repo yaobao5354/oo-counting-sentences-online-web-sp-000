b17ef0d3d46df872e6258f28f9deb5f776ece7fd
yaoyu bao <yaobao5354@gmail.com>
2019-08-29 20:22:12 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else 
+      false 
+    end 
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else 
+      false 
+    end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else 
+      false 
+    end 
   end
 
   def count_sentences
-
+    counter = 0
+    string = self.split
+    string.each do |word|
+      if word.include?(".")
+        counter +=1
+      elsif word.include?("?")
+        counter += 1
+      elsif word.include?("!")
+        counter += 1
+      end 
+    end 
+    return counter
   end
 end
